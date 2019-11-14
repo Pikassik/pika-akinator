1388cc66e69196bedfa17f83b4e164ba1e6c9f21
Pikassik <pikacco.ya@yandex.ru>
2019-11-14 03:07:24 +0300

    difference in process

--- headers/Akinator.h
+++ headers/Akinator.h
@@ -11,17 +11,20 @@ class Akinator {
   void WriteFile(const std::string& filename) const;
   void InteractiveMode();
   void TraversalMode();
+  void DifferenceMode();
  private:
   struct Node {
     std::string_view string;
-    size_t left = 0;
-    size_t right = 0;
+    size_t left   = 0; // true  subtree
+    size_t right  = 0; // false subtree
     size_t parent = 0;
   };
 
   void Reserve(std::FILE* file);
   void BuildTree(std::FILE* file);
   void UpdateTree(size_t current_node);
+  std::vector<Node>::const_iterator ReadAndFindCharacter();
+  size_t NodeDepth(size_t node) const;
   void CreateRoot();
   inline bool IsLeaf(const Node& node) const;
   std::vector<Node> tree_;
--- sources/Akinator.cpp
+++ sources/Akinator.cpp
@@ -70,39 +70,27 @@ void Akinator::WriteFile(const std::string& filename) const {
 }
 
 void Akinator::InteractiveMode() {
-  size_t current_node = 0;
   while (true) {
-    if (!IsLeaf(tree_.at(current_node))) {
-      std::printf("%.*s (yes or no):", tree_.at(current_node).string.size(),
-                                   tree_.at(current_node).string.data());
-      while (std::isspace(std::cin.peek())) std::cin.get();
-      std::getline(std::cin, input_buffer_);
-      if (input_buffer_ == "y") {
-        current_node = tree_.at(current_node).left;
-      } else if (input_buffer_ == "n") {
-        current_node = tree_.at(current_node).right;
-      } else {
-        std::cout << "Please, answer y or n";
-      }
-      continue;
-    }
-
-    std::printf("Is it %.*s?\n", tree_.at(current_node).string.size(),
-                                 tree_.at(current_node).string.data());
-    std::fflush(stdout);
+    std::printf("Available modes:\n"
+                "traversal\n"
+                "difference\n"
+                "write tree\n"
+                "show tree\n"
+                "Write one to start (or exit):");
     while (std::isspace(std::cin.peek())) std::cin.get();
     std::getline(std::cin, input_buffer_);
-    if (input_buffer_ == "y") {
-      printf("Easy.\n");
+
+    if        (input_buffer_ == "traversal") {
+      TraversalMode();
+    } else if (input_buffer_ == "difference") {
+
+    } else if (input_buffer_ == "write tree") {
+
+    } else if (input_buffer_ == "show tree") {
+
+    } else if (input_buffer_ == "exit") {
       return;
-    } else if (input_buffer_ == "n") {
-      UpdateTree(current_node);
-      printf("Thank you!\n");
-    } else {
-      std::cout << "Please, answer y or n\n";
-      continue;
     }
-    break;
   }
 }
 
@@ -139,6 +127,42 @@ void Akinator::TraversalMode() {
       continue;
     }
   }
+}
+
+void Akinator::DifferenceMode() {
+  auto find_node = [this]() {
+    auto it = ReadAndFindCharacter();
+    if (it == tree_.end())
+      printf("there is no this character\n");
+    return it;
+  };
+
+  std::printf("Enter first character:");
+  auto first_node_it = find_node();
+  if (first_node_it == tree_.cend()) return;
+  size_t first_node = std::distance(tree_.cbegin(), first_node_it);
+  std::printf("Enter second character:");
+  auto second_node_it = find_node();
+  if (second_node_it == tree_.cend()) return;
+  size_t second_node = std::distance(tree_.cbegin(), first_node_it);
+  size_t first_depth = NodeDepth(first_node);
+  size_t second_depth = NodeDepth(second_node);
+  bool second_deeper = false;
+  if (second_depth > first_depth) {
+    second_deeper = true;
+    std::swap(first_depth, second_depth);
+    std::swap(first_node, second_node);
+  }
+  for (size_t i = 0; i < first_depth - second_depth; ++i) {
+    first_node = tree_.at(first_node).parent;
+  }
+  if (second_deeper) std::swap(first_node, second_node);
+  while (first_node != second_node) {
+    first_node  = tree_.at(first_node).parent;
+    second_node = tree_.at(second_node).parent;
+  }
+
+
 }
 
 void Akinator::Reserve(std::FILE* file) {
@@ -222,6 +246,25 @@ void Akinator::BuildTree(std::FILE* file) {
   traversal_stack.pop_back();
 }
 
+std::vector<Akinator::Node>::const_iterator Akinator::ReadAndFindCharacter() {
+  while (std::isspace(std::cin.peek())) std::cin.get();
+  std::getline(std::cin, input_buffer_);
+  return std::find_if(tree_.begin(), tree_.end(),
+                      [this](const Node& node) {
+    return std::string_view(input_buffer_) == node.string &&
+           IsLeaf(node);
+  });
+}
+
+size_t Akinator::NodeDepth(size_t node) const {
+  size_t depth = 0;
+  while (node != 0) {
+    node = tree_.at(node).parent;
+    ++depth;
+  }
+  return depth;
+}
+
 void Akinator::CreateRoot() {
   nodes_strings_ += kUnknown;
   tree_.push_back({std::string_view(nodes_strings_).
@@ -245,8 +288,8 @@ void Akinator::UpdateTree(size_t current_node) {
     tree_.at(tree_.at(current_node).parent).right) = tree_.size();
   tree_.push_back({static_cast<std::string_view>(nodes_strings_).
                    substr(begin, input_buffer_.size() + 1),
-                   tree_.size() + 1, current_node, // ?
-                   tree_.at(current_node).parent}); // here i need parent
+                   tree_.size() + 1, current_node,
+                   tree_.at(current_node).parent});
   tree_.at(current_node).parent = tree_.size() - 1;
   begin = nodes_strings_.size();
   printf("Please, write your character:");
@@ -258,4 +301,3 @@ void Akinator::UpdateTree(size_t current_node) {
                    0, 0,
                    tree_.size() - 1});
 }
-
